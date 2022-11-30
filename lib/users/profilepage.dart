f15edd163a05058e020c435ee09a2aaa5ad5a488
Aldi Saputra Rahmawan <11191007@student.itk.ac.id>
2022-11-30 15:51:44 +0800

    Finished profile page

--- lib/users/profilepage.dart
+++ lib/users/profilepage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:glowskin_project/utils/user_preferences.dart';
 import 'package:glowskin_project/widgets/profile_widget.dart';
 import 'package:glowskin_project/users/editprofilepage.dart';
+import 'package:dio/dio.dart';
+import 'dart:io';
 
 
 class ProfilPage extends StatefulWidget {
@@ -14,10 +16,27 @@ class ProfilPage extends StatefulWidget {
 class _ProfilPageState extends State<ProfilPage> {
   @override
 
+  String url = Platform.isAndroid ? "http://192.168.137.37:3001" : 'http://localhost:3001';
+
+  Future getUser() async {
+    try{
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String token = prefs.getString('token')!;
+      String email = prefs.getString('email')!;
+      var dio = Dio();
+      dio.options.headers['content-Type'] = 'application/json';
+      dio.options.headers["accept"] = "application/json";
+      var response = await dio.get(url + '/user/get-user-by-email/$email');
+      print(response.data);
+      return response.data;
+    }catch(e){
+      print(e);
+    }
+  }
+
   Widget build(BuildContext context) {
-
     final user = UserPreferences.myUser;
-
+    getUser();
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(10),
@@ -73,53 +92,62 @@ class _ProfilPageState extends State<ProfilPage> {
             ),
             Container(
               padding: EdgeInsets.only(top: 50),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(right: 130),
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      "Name",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(right: 60),
-                    padding: EdgeInsets.only(bottom: 30),
-                    child: Text(
-                      user.name,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(right: 130),
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      "Email",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(right: 10),
-                    padding: EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      user.email,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                  ),
-                ],
+              child: FutureBuilder(
+                future: getUser(),
+                builder: (context, snapshot){
+                  if(snapshot.hasData){
+                    return Column(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(right: 130),
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            "Name",
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 150),
+                          padding: EdgeInsets.only(bottom: 30),
+                          child: Text(
+                            snapshot.data['name'],
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 130),
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            "Email",
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 50),
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            snapshot.data['email'],
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }else{
+                    return Center(child: CircularProgressIndicator());
+                  }
+                },
               ),
             ),
           ],
--- lib/utils/user_preferences.dart
+++ lib/utils/user_preferences.dart
@@ -3,7 +3,7 @@ import 'package:glowskin_project/model/user.dart';
 class UserPreferences {
   static const myUser = User(
     imagePaths: 
-    'https://sm.askmen.com/t/askmen_in/article/f/facebook-p/facebook-profile-picture-affects-chances-of-gettin_fr3n.1200.jpg', 
+    'https://www.pngitem.com/pimgs/m/111-1114669_blank-person-hd-png-download.png', 
     name: 'GlowSkin Admin', 
     email: 'GlowSkin@gmail.com');
 }
